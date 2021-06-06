55c08106fc509905c3f6a6ef0e8503e2c20185e1
fcesc-code <bpfran@gmail.com>
2021-06-06 21:10:23 +0200

    mockdata / updated new mockdata

---
+++ winedata.js
@@ -0,0 +1,674 @@
+module.exports = [
+  {
+      "_id": 1,
+      "name": "CHATEAU DE SAINT COSME",
+      "year": 2009,
+      "rating": 5,
+      "grapes": "Grenache / Syrah",
+      "country": "France",
+      "region": "Southern Rhone",
+      "description": "The aromas of fruit and spice give one a hint of the light drinkability of this lovely wine, which makes an excellent complement to fish dishes.",
+      "price": 16.75, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 2,
+      "name": "LAN RIOJA CRIANZA",
+      "year": 2006,
+      "rating": 4,
+      "grapes": "Tempranillo",
+      "country": "Spain",
+      "region": "Rioja",
+      "description": "A resurgence of interest in boutique vineyards has opened the door for this excellent foray into the dessert wine market. Light and bouncy, with a hint of black truffle, this wine will not fail to tickle the taste buds.",
+      "price": 19.10, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 3,
+      "name": "MARGERUM SYBARITE",
+      "year": 2010,
+      "rating": 3,
+      "grapes": "Sauvignon Blanc",
+      "country": "USA",
+      "region": "California Central Cosat",
+      "description": "The cache of a fine Cabernet in ones wine cellar can now be replaced with a childishly playful wine bubbling over with tempting tastes of black cherry and licorice. This is a taste sure to transport you back in time.",
+      "price": 11.70, 
+      "isOnSale": false, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 4,
+      "name": "OWEN ROE \"EX UMBRIS\"",
+      "year": 2009,
+      "rating": 2.75,
+      "grapes": "Syrah",
+      "country": "USA",
+      "region": "Washington",
+      "description": "A one-two punch of black pepper and jalapeno will send your senses reeling, as the orange essence snaps you back to reality. Don't miss this award-winning taste sensation.",
+      "price": 10.10, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 5,
+      "name": "REX HILL",
+      "year": 2009,
+      "rating": 4,
+      "grapes": "Pinot Noir",
+      "country": "USA",
+      "region": "Oregon",
+      "description": "One cannot doubt that this will be the wine served at the Hollywood award shows, because it has undeniable star power. Be the first to catch the debut that everyone will be talking about tomorrow.",
+      "price": 20.00, 
+      "isOnSale": false, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 6,
+      "name": "VITICCIO CLASSICO RISERVA",
+      "year": 2007,
+      "rating": 5,
+      "grapes": "Sangiovese Merlot",
+      "country": "Italy",
+      "region": "Tuscany",
+      "description": "Though soft and rounded in texture, the body of this wine is full and rich and oh-so-appealing. This delivery is even more impressive when one takes note of the tender tannins that leave the taste buds wholly satisfied.",
+      "price": 18.45, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 7,
+      "name": "CHATEAU LE DOYENNE",
+      "year": 2005,
+      "rating": 4.6,
+      "grapes": "Merlot",
+      "country": "France",
+      "region": "Bordeaux",
+      "description": "Though dense and chewy, this wine does not overpower with its finely balanced depth and structure. It is a truly luxurious experience for the senses.",
+      "price": 16.90, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 8,
+      "name": "DOMAINE DU BOUSCAT",
+      "year": 2009,
+      "rating": 4.85,
+      "grapes": "Merlot",
+      "country": "France",
+      "region": "Bordeaux",
+      "description": "The light golden color of this wine belies the bright flavor it holds. A true summer wine, it begs for a picnic lunch in a sun-soaked vineyard.",
+      "price": 18.00, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 9,
+      "name": "BLOCK NINE",
+      "year": 2009,
+      "rating": 3.7,
+      "grapes": "Pinot Noir",
+      "country": "USA",
+      "region": "California",
+      "description": "With hints of ginger and spice, this wine makes an excellent complement to light appetizer and dessert fare for a holiday gathering.",
+      "price": 15.90, 
+      "isOnSale": false, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 10,
+      "name": "DOMAINE SERENE",
+      "year": 2007,
+      "rating": 3.6,
+      "grapes": "Pinot Noir",
+      "country": "USA",
+      "region": "Oregon",
+      "description": "Though subtle in its complexities, this wine is sure to please a wide range of enthusiasts. Notes of pomegranate will delight as the nutty finish completes the picture of a fine sipping experience.",
+      "price": 13.00, 
+      "isOnSale": false, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 11,
+      "name": "BODEGA LURTON",
+      "year": 2011,
+      "rating": 4.35,
+      "grapes": "Pinot Gris",
+      "country": "Argentina",
+      "region": "Mendoza",
+      "description": "Solid notes of black currant blended with a light citrus make this wine an easy pour for varied palates.",
+      "price": 13.95, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 12,
+      "name": "LES MORIZOTTES",
+      "year": 2009,
+      "rating": 4.4,
+      "grapes": "Chardonnay",
+      "country": "France",
+      "region": "Burgundy",
+      "description": "Breaking the mold of the classics, this offering will surprise and undoubtedly get tongues wagging with the hints of coffee and tobacco in perfect alignment with more traditional notes. Sure to please the late-night crowd with the slight jolt of adrenaline it brings.",
+      "price": 14.15, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 13,
+      "name": "ARGIANO NON CONFUNDITUR",
+      "year": 2009,
+      "rating": 4.65,
+      "grapes": "Cabernet Sauvignon",
+      "country": "Italy",
+      "region": "Tuscany",
+      "description": "Like a symphony, this cabernet has a wide range of notes that will delight the taste buds and linger in the mind.",
+      "price": 16.85, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 14,
+      "name": "DINASTIA VIVANCO ",
+      "year": 2008,
+      "rating": 4.7,
+      "grapes": "Tempranillo",
+      "country": "Spain",
+      "region": "Rioja",
+      "description": "Whether enjoying a fine cigar or a nicotine patch, don't pass up a taste of this hearty Rioja, both smooth and robust.",
+      "price": 17.50, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 15,
+      "name": "PETALOS BIERZO",
+      "year": 2009,
+      "rating": 4.55,
+      "grapes": "Mencia",
+      "country": "Spain",
+      "region": "Castilla y Leon",
+      "description": "For the first time, a blend of grapes from two different region's have been combined in an outrageous explosion of flavor that cannot be missed.",
+      "price": 19.90, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 16,
+      "name": "SHAFER RED SHOULDER RANCH",
+      "year": 2009,
+      "rating": 4.2,
+      "grapes": "Chardonnay",
+      "country": "USA",
+      "region": "California",
+      "description": "Keep an eye out for this winery in coming year's, as their chardonnays have reached the peak of perfection.",
+      "price": 15.35, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 17,
+      "name": "PONZI",
+      "year": 2010,
+      "rating": 3.85,
+      "grapes": "Pinot Gris",
+      "country": "USA",
+      "region": "Oregon",
+      "description": "For those who appreciate the simpler pleasures in life, this light pinot grigio will blend perfectly with a light meal or as an after dinner drink.",
+      "price": 10.00, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 18,
+      "name": "HUGEL",
+      "year": 2010,
+      "rating": 3.9,
+      "grapes": "Pinot Gris",
+      "country": "France",
+      "region": "Alsace",
+      "description": "Fresh as new buds on a spring vine, this dewy offering is the finest of the new generation of pinot grigios.  Enjoy it with a friend and a crown of flowers for the ultimate wine tasting experience.",
+      "price": 17.20, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 19,
+      "name": "FOUR VINES MAVERICK",
+      "year": 2011,
+      "rating": 3,
+      "grapes": "Zinfandel",
+      "country": "USA",
+      "region": "California",
+      "description": "o yourself a favor and have a bottle (or two) of this fine zinfandel on hand for your next romantic outing.  The only thing that can make this fine choice better is the company you share it with.",
+      "price": 13.40, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 20,
+      "name": "QUIVIRA DRY CREEK VALLEY",
+      "year": 2009,
+      "rating": 2.9,
+      "grapes": "Zinfandel",
+      "country": "USA",
+      "region": "California",
+      "description": "Rarely do you find a zinfandel this oakey from the Sonoma region. The vintners have gone to extremes to duplicate the classic flavors that brought high praise in the early '90s.",
+      "price": 9.85, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 21,
+      "name": "CALERA 35TH ANNIVERSARY",
+      "year": 2010,
+      "rating": 4,
+      "grapes": "Pinot Noir",
+      "country": "USA",
+      "region": "California",
+      "description": "Fruity and bouncy, with a hint of spice, this pinot noir is an excellent candidate for best newcomer from Napa this year.",
+      "price": 14.60, 
+      "isOnSale": false, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 22,
+      "name": "CHATEAU CARONNE STE GEMME",
+      "year": 2010,
+      "rating": 4.95,
+      "grapes": "Cabernet Sauvignon",
+      "country": "France",
+      "region": "Bordeaux",
+      "description": "Find a sommelier with a taste for chocolate and he's guaranteed to have this cabernet on his must-have list.",
+      "price": 24.15, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 22,
+      "name": "MOMO MARLBOROUGH",
+      "year": 2010,
+      "rating": 2.8,
+      "grapes": "Sauvignon Blanc",
+      "country": "New Zealand",
+      "region": "South Island",
+      "description": "Best served chilled with melon or a nice salty prosciutto, this sauvignon blanc is a staple in every Italian kitchen, if not on their wine list.  Request the best, and you just may get it.",
+      "price": 15.50, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  },
+  {
+      "_id": 23,
+      "name": "WATERBROOK",
+      "year": 2009,
+      "rating": 4.15,
+      "grapes": "Merlot",
+      "country": "USA",
+      "region": "Washington",
+      "description": "Legend has it the gods didn't share their ambrosia with mere mortals.  This merlot may be the closest we've ever come to a taste of heaven.",
+      "price": 10.15, 
+      "isOnSale": true, 
+      "quantityInCart": 0, 
+      "foodMatch": [
+    {
+      "name": "Manchego cheese",
+      "kcal": 320,
+      "vegan": false,
+      "gluten": false
+    },
+    {
+      "name": "Jamón Ibérico",
+      "kcal": 450,
+      "vegan": false,
+      "gluten": false
+    }
+  ], 
+      "imageUrl": "wine_example.png"
+  }
+]
--- wines.js
+++ wines.js
@@ -1,74 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const wines = [
-  {
-    id: 1,
-    name: 'Domaine de la Butte La Pied de la Butte',
-    imageUrl: 'assets/images/wine1.jpg',
-    price: 19.95,
-    foodPairing: [
-      {
-        name: 'Embutidos',
-        glutten: false,
-        kcal: 600,
-        vegan: false
-      },
-      {
-        name: 'Patés',
-        glutten: false,
-        kcal: 200,
-        vegan: false
-      }
-    ],
-    isOnSale: false,
-    quantityInCart: 0
-  },
-  {
-    id: 2,
-    name: 'Lolo',
-    imageUrl: 'assets/images/wine2.jpg',
-    price: 6.15,
-    foodPairing: [
-      {
-        name: 'Arroces caldosos',
-        glutten: true,
-        kcal: 455,
-        vegan: true
-      },
-      {
-        name: 'Arroces de pescado',
-        glutten: true,
-        kcal: 564,
-        vegan: false
-      }
-    ],
-    isOnSale: true,
-    quantityInCart: 0
-  },
-  {
-    id: 3,
-    name: 'Pago de Carraovejas',
-    imageUrl: 'assets/images/wine3.jpg',
-    price: 31.9,
-    foodPairing: [
-      {
-        name: 'Carnes rojas asadas',
-        glutten: false,
-        kcal: 532,
-        vegan: false
-      },
-      {
-        name: 'Quesos curados',
-        glutten: false,
-        kcal: 330,
-        vegan: true
-      }
-    ],
-    isOnSale: false,
-    quantityInCart: 0
-  }
-];
+const wines = require('./winedata');
 
 router.get('/', (req, res) => {
   var query = (req.query['q'] || '').toLowerCase();
