c9adb6cf4e47b14dca9b904c69f9d72c1b5b429c
fcesc-code <bpfran@gmail.com>
2021-06-24 22:21:11 +0200

    hotfix / updated create route backend

--- wineRouter.js
+++ wineRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const wineRouter = express.Router();
+const chalk = require('chalk');
 
 const winesData = require('./WINEDATA');
 let wines = [ ...winesData ];
 
 wineRouter.get('/', (req, res) => {
   const query = (req.query['q'] || '').toLowerCase();
+  console.log(`/api/wine/'${chalk.green(`route called with get, query: '${query}'`)}`)
   if (query) {
     const foundWines = wines.filter(
       ({ name }) => name.toLowerCase().indexOf(query) !== -1
@@ -17,6 +19,7 @@ wineRouter.get('/', (req, res) => {
 
 wineRouter.get('/page/:pageNumber', (req, res) => {
   const page = +req.params.pageNumber;
+  console.log(`/api/wine/page/:pageNumber'${chalk.green(`route called with get, page: ${page}`)}`)
   const itemsPerPage = 10;
   const start = (pageNumber - 1) * itemsPerPage;
   const end = start + itemsPerPage;
@@ -27,8 +30,9 @@ wineRouter.get('/page/:pageNumber', (req, res) => {
     : res.status(400).json({msg: `Wine with id: ${page} not found!`});
 });
 
-wineRouter.get(`/:code`, (req, res) => {
-  const wineParameter = +req.params.code;
+wineRouter.get(`/:id`, (req, res) => {
+  const wineId = +req.params.id;
+  console.log(`/api/wine/:id'${chalk.green(`route called with get, ${wineId}`)}`)
   const filteredWines = wines.find( ({ _id }) => +_id === wineParameter);
 
   return (filteredWines) 
@@ -40,6 +44,7 @@ function routerWithParams( parameter ){
   return (
       wineRouter.get(`/${parameter}/:value`, (req, res) => {
       const wineParameter = String(req.params.value);
+      console.log(`/api/wine/${parameter}/:value' ${chalk.green(`route called with get, ${wineParameter}`)}`)
       const filteredWines = wines.filter( wine => String(wine[parameter]) === wineParameter);
 
       return (filteredWines) 
@@ -57,11 +62,16 @@ routerWithParams('grapes');
 
 wineRouter.post('/', (req, res) => {
   let wine = req.body;
-  if (!wine._id) {
+  wine.name = wine.name.trim().toUpperCase();
+  const foundWine = wines.findIndex( ({ name }) => name === wine.name );
+  if (foundWine === -1) {
     wine = ({ ...wine, quantityInCart: 0, _id: wines.length + 1})
+    const DEFAULT_IMGURL = 'wine_example.png';
+    // if( wine.imgUrl.trim() === '' ) wine.imgUrl = DEFAULT_IMGURL;
+    wine.imgUrl = DEFAULT_IMGURL;
     wines.push(wine);
   };
-  return (wine._id) 
+  return (foundWine !== -1) 
     ? res
       .status(400)
       .json({ msg: `Wine seems to already have an id assigned` })
@@ -72,6 +82,7 @@ wineRouter.post('/', (req, res) => {
 
 wineRouter.patch('/:id', (req, res) => {
   const wineId = +req.params._id;
+  console.log(`/api/wine/:id'${chalk.green(`route called with patch, ${wineId}`)}`)
   const foundWine = wines.find(({ _id }) => _id === wineId);
   if (foundWine) {
     const changeInQuantity = +req.body.changeInQuantity;
@@ -87,6 +98,7 @@ wineRouter.patch('/:id', (req, res) => {
 });
 
 wineRouter.patch('/edit/', (req, res) => {
+  console.log(`/api/wine/edit/'${chalk.green('route called with patch')}`)
   let updatedWine = req.body;
   if (updatedWine._id) {
     foundWine = wines.find( ({ _id}) => _id === updatedWine._id );
@@ -105,6 +117,7 @@ wineRouter.patch('/edit/', (req, res) => {
 
 wineRouter.delete('/:id', (req, res) => {
   const wineId = +req.params._id;
+  console.log(`/api/wine/:id'${chalk.green(`route called with delete, ${wineId}`)}`)
   newWines = [ ...wines ].filter(({ _id }) => _id !== wineId);
   wines = newWines;
   return res.status(200).json({ msg: `Wine with id:${wineId} successfully deleted.` })
